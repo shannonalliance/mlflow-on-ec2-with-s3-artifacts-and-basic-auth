e036cd67c34d9d5de7b41a8bea55759119452764
Kevin Bolger <kevinwtbolger@gmail.com>
2024-05-29 21:48:41 -0700

    Formatting changes

--- terraform/mlflow/ec2_storage/variables.tf
+++ terraform/mlflow/ec2_storage/variables.tf
@@ -1,13 +1,13 @@
 variable "mlflow_ec2_subnet_az" {
-    description = "The availability zone to use for subnet 1, where the EC2 instance will reside."
-    type = string
+  description = "The availability zone to use for subnet 1, where the EC2 instance will reside."
+  type        = string
 }
 
 variable "mlflow_ec2_persistent_ebs_volume_size" {
-    description = "The amount of storage in GiB to allocated the externally mounted EBS volume."
-    type = string
+  description = "The amount of storage in GiB to allocated the externally mounted EBS volume."
+  type        = string
 }
 
-variable "cost_purpose_tag_value"{
-    description = "Value to be assigned to the 'CostPurpose' tag across resources."
+variable "cost_purpose_tag_value" {
+  description = "Value to be assigned to the 'CostPurpose' tag across resources."
 }
