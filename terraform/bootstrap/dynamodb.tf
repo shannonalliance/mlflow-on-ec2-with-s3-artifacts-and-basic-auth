3d47ef9325d6054a0ca2e4b9c4c5a5b08a894635
Kevin Bolger <kevinwtbolger@gmail.com>
2024-05-29 21:49:27 -0700

    Formatting changes

--- terraform/bootstrap/dynamodb.tf
+++ terraform/bootstrap/dynamodb.tf
@@ -1,22 +1,22 @@
 resource "aws_dynamodb_table" "defect-detector-terraform_state" {
-    name = var.dynamodb_state_lock_name
-    hash_key = "LockID"
-    billing_mode = "PAY_PER_REQUEST"
+  name         = var.dynamodb_state_lock_name
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
 
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
-      deletion_protection_enabled = true
+  deletion_protection_enabled = true
 
-    server_side_encryption {
-        enabled = true
-        kms_key_arn = aws_kms_key.mlflow_server_state_resources_key.arn
-    }
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.mlflow_server_state_resources_key.arn
+  }
 
-    tags = {
-        Name = "mlflow_state_lock"
-        Purpose = "mlfow_for_teams_demo"
-    }
+  tags = {
+    Name    = "mlflow_state_lock"
+    Purpose = "mlfow_for_teams_demo"
+  }
 }
--- terraform/bootstrap/s3.tf
+++ terraform/bootstrap/s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "mlflow_state_bucket" {
-    bucket = var.state_bucket_name
-    
-    tags = {
-        Name = "mlflow_state_bucket"
-        Purpose = "mlfow_for_teams_demo"
-    }
+  bucket = var.state_bucket_name
+
+  tags = {
+    Name    = "mlflow_state_bucket"
+    Purpose = "mlfow_for_teams_demo"
+  }
 }
