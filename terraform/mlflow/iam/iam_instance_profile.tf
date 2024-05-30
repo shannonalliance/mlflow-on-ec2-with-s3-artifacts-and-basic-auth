68f1e0d20a5979f7c526837596a7b665c0c5752c
Kevin Bolger <kevinwtbolger@gmail.com>
2024-05-29 21:45:45 -0700

    Formatting changes

--- terraform/mlflow/iam/iam_instance_profile.tf
+++ terraform/mlflow/iam/iam_instance_profile.tf
@@ -1,9 +1,9 @@
 resource "aws_iam_instance_profile" "mlflow_instance_profile_for_artifacts_bucket_access" {
-    name = "mlflow_instance_profile_for_artifacts_bucket_access"
-    role = aws_iam_role.mlflow_iam_role_for_artifacts_bucket_access.name
-    
-    tags = {
-        Name = "mlflow_instance_profile_for_artifacts_bucket_access"
-        CostPurpose = var.cost_purpose_tag_value
-    }
+  name = "mlflow_instance_profile_for_artifacts_bucket_access"
+  role = aws_iam_role.mlflow_iam_role_for_artifacts_bucket_access.name
+
+  tags = {
+    Name        = "mlflow_instance_profile_for_artifacts_bucket_access"
+    CostPurpose = var.cost_purpose_tag_value
+  }
 }
--- terraform/mlflow/iam/iam_role.tf
+++ terraform/mlflow/iam/iam_role.tf
@@ -1,22 +1,22 @@
 resource "aws_iam_role" "mlflow_iam_role_for_artifacts_bucket_access" {
-    name = "mlflow_iam_role_for_artifacts_bucket_access"
+  name = "mlflow_iam_role_for_artifacts_bucket_access"
 
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = "sts:AssumeRole"
-                Principal = {
-                    Service = "ec2.amazonaws.com"
-                }
-                Effect = "Allow"
-                Sid    = ""
-            },
-        ]
-    })
-    
-    tags = {
-        Name = "mlflow_iam_role_for_artifacts_bucket_access"
-        CostPurpose = var.cost_purpose_tag_value
-    }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      },
+    ]
+  })
+
+  tags = {
+    Name        = "mlflow_iam_role_for_artifacts_bucket_access"
+    CostPurpose = var.cost_purpose_tag_value
+  }
 }
