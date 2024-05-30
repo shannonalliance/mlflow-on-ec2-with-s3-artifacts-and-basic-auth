5faf4cf2b12ab7083641ef497301ae99954f4b2b
Kevin Bolger <kevinwtbolger@gmail.com>
2024-05-29 21:46:17 -0700

    Formatting changes

--- terraform/mlflow/mlflow_server/ansible_provisioner.tf
+++ terraform/mlflow/mlflow_server/ansible_provisioner.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "ansible_provisioner" {
 
-    provisioner "local-exec" {
-        command = "ansible-playbook --private-key=${var.private_key_path} -i ${path.module}/ansible/hosts.ini ${path.module}/ansible/start_mlflow_server.yml"
-    }
+  provisioner "local-exec" {
+    command = "ansible-playbook --private-key=${var.private_key_path} -i ${path.module}/ansible/hosts.ini ${path.module}/ansible/start_mlflow_server.yml"
+  }
 }
--- terraform/mlflow/mlflow_server/variables.tf
+++ terraform/mlflow/mlflow_server/variables.tf
@@ -1,4 +1,4 @@
-variable "private_key_path"{
-    description="The path to the private key for ssh-ing in to the EC2 instance."
-    type = string
+variable "private_key_path" {
+  description = "The path to the private key for ssh-ing in to the EC2 instance."
+  type        = string
 }
