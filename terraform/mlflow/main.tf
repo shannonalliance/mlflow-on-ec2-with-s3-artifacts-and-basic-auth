6a6011cd207dfc0623ef93ea3ef56cdcf275371d
Kevin Bolger <kevinwtbolger@gmail.com>
2024-05-29 21:48:52 -0700

    Remove unnecessary variables from load balancer module
    The redundant variables being passed into the load balancer module in the 'mlflow' terraform code have been stripped out. Only 'hosted_zone_id' is now passed into the module. It streamlines the code and makes it easier to understand.

--- terraform/mlflow/main.tf
+++ terraform/mlflow/main.tf
@@ -1,63 +1,55 @@
 module "network" {
-    source = "./network"
+  source = "./network"
 
-    aws_region = var.aws_region
-    mlflow_ec2_subnet_az = var.mlflow_ec2_subnet_az
-    mlflow_load_balancer_subnet_az = var.mlflow_load_balancer_subnet_az
-    cost_purpose_tag_value = var.cost_purpose_tag_value
+  aws_region                     = var.aws_region
+  mlflow_ec2_subnet_az           = var.mlflow_ec2_subnet_az
+  mlflow_load_balancer_subnet_az = var.mlflow_load_balancer_subnet_az
+  cost_purpose_tag_value         = var.cost_purpose_tag_value
 }
 
 module "s3_bucket" {
-    source = "./s3_bucket"
+  source = "./s3_bucket"
 
-    mlflow_artifacts_bucket_name = var.mlflow_artifacts_bucket_name
-    cost_purpose_tag_value = var.cost_purpose_tag_value
+  mlflow_artifacts_bucket_name = var.mlflow_artifacts_bucket_name
+  cost_purpose_tag_value       = var.cost_purpose_tag_value
 }
 
 module "iam" {
-    source = "./iam"
+  source = "./iam"
 
-    mlflow_artifacts_bucket_name = var.mlflow_artifacts_bucket_name
-    cost_purpose_tag_value = var.cost_purpose_tag_value
+  mlflow_artifacts_bucket_name = var.mlflow_artifacts_bucket_name
+  cost_purpose_tag_value       = var.cost_purpose_tag_value
 }
 
 module "ec2_storage" {
-    source = "./ec2_storage"
+  source = "./ec2_storage"
 
-    mlflow_ec2_subnet_az = var.mlflow_ec2_subnet_az
-    mlflow_ec2_persistent_ebs_volume_size = var.mlflow_ec2_persistent_ebs_volume_size
-    cost_purpose_tag_value = var.cost_purpose_tag_value
+  mlflow_ec2_subnet_az                  = var.mlflow_ec2_subnet_az
+  mlflow_ec2_persistent_ebs_volume_size = var.mlflow_ec2_persistent_ebs_volume_size
+  cost_purpose_tag_value                = var.cost_purpose_tag_value
 }
 
 module "ec2_instance" {
-    source = "./ec2_instance"
+  source = "./ec2_instance"
 
-    mlflow_ec2_persistent_ebs_volume_id = module.ec2_storage.mlflow_ec2_persistent_ebs_volume_id
-    ec2_instance_type = var.ec2_instance_type
-    ec2_ami = var.ec2_ami
-    mlflow_ec2_subnet_id = module.network.mlflow_ec2_subnet_id
-    mlflow_default_security_group_id = module.network.mlflow_default_security_group_id
-    mlflow_instance_profile_for_artifacts_bucket_access_name = module.iam.mlflow_instance_profile_for_artifacts_bucket_access_name
-    mlflow_ec2_eip_id = module.network.mlflow_ec2_eip_id
-    public_key_path = var.public_key_path
-    cost_purpose_tag_value = var.cost_purpose_tag_value
+  mlflow_ec2_persistent_ebs_volume_id                      = module.ec2_storage.mlflow_ec2_persistent_ebs_volume_id
+  ec2_instance_type                                        = var.ec2_instance_type
+  ec2_ami                                                  = var.ec2_ami
+  mlflow_ec2_subnet_id                                     = module.network.mlflow_ec2_subnet_id
+  mlflow_default_security_group_id                         = module.network.mlflow_default_security_group_id
+  mlflow_instance_profile_for_artifacts_bucket_access_name = module.iam.mlflow_instance_profile_for_artifacts_bucket_access_name
+  mlflow_ec2_eip_id                                        = module.network.mlflow_ec2_eip_id
+  public_key_path                                          = var.public_key_path
+  cost_purpose_tag_value                                   = var.cost_purpose_tag_value
 }
 
 module "mlflow_server" {
-    source = "./mlflow_server"
-    
-    private_key_path=var.private_key_path
+  source = "./mlflow_server"
+
+  private_key_path = var.private_key_path
 }
 
 module "load_balancer" {
-    source = "./load_balancer"
-
-    mlflow_ec2_persistent_ebs_volume_id = module.ec2_storage.mlflow_ec2_persistent_ebs_volume_id
-    hosted_zone_id = var.hosted_zone_id
-    mlflow_default_security_group_id = module.network.mlflow_default_security_group_id
-    mlflow_ec2_subnet_id = module.network.mlflow_ec2_subnet_id
-    mlflow_load_balancer_subnet_id = module.network.mlflow_load_balancer_subnet_id
-    mlflow_vpc_id = module.network.mlflow_vpc_id
-    mlflow_instance_id = module.ec2_instance.mlflow_instance_id
-    cost_purpose_tag_value = var.cost_purpose_tag_value
+  source         = "./load_balancer"
+  hosted_zone_id = var.hosted_zone_id
 }
